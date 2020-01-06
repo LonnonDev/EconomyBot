f1e3b9cda7aeae0de07a11657beaa92ede2b1da4
LonnonjamesD <xxl3m0nk1ngxx@gmail.com>
2020-01-06 17:34:39 -0500

    2.1.1

--- options.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-#====================#
-# Options
-shardids = 1
-shardcount = 1
-initial_extensions = ['cogs.generalcommands']
-commandprefix = ('f!', 'f! ', 'f1', 'f1 ')
-#====================#
